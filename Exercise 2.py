5e7b87b974ef505b033e30d0ef61d34bd6c8ace6
Shane <gupitshanehazel@gmail.com>
2023-05-01 11:53:44 +0800

    putting of method function

--- Exercise 2.py
+++ Exercise 2.py
@@ -2,29 +2,12 @@
 
 # make a method
 def process ():
-
     # open students.txt file (read)
-    with open("students.txt") as input_file:
-
         # assign variables for highest GWA and the student with highest grade
-        high_gwa = 2
-        high_student = ""
-
         # read students.txt line by line
-        for line in input_file:
-
             # separate the names and their respective GWA
-            names, gwa = line.strip().split(" = ")
-
             # make the GWA a float
-            gwa = float(gwa)
-
             # if the current gwa is higher than the highest gwa, update the variables
-            if gwa < high_gwa:
-                high_gwa = gwa
-                high_student = names
-
         # print
-        print(high_student, "got the highest GWA of", high_gwa)
-
-process()
+# end of method
+process ()
